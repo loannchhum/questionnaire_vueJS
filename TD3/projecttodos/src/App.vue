028401e8d7fed5c29d73f8766dbf901ffcadc8a8
Chhum--Moxel Loann <o22204226@info23-10.iut45.univ-orleans.fr>
2024-03-27 08:23:57 +0100

    first commit

---
+++ TD3/projecttodos/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import TodoItem from './components/TodoItem.vue';
+
+let data = [
+    { id: "1", text: 'Apprendre ES6 ', checked: true },
+    { id: "2", text: 'Apprendre Vue ', checked: false },
+    { id: "3", text: 'Faire un projet magnifique ', checked: false }
+    ]
+
+export default {
+    data() {
+      return {todos:[
+    { text: 'Apprendre ES6 ', checked: true },
+    { text: 'Apprendre Vue ', checked: false },
+    { text: 'Faire un projet magnifique ', checked: false }
+    ]};
+    },
+    methods:{
+        remove(index) {
+          this.todos.splice(index,1);
+        }
+    },
+
+    components : { TodoItem }
+}
+
+</script>
+
+<template>
+  <h1>Todos</h1>
+  <ul>
+    <button @click = remove()>Ajouter une tâche</button >
+    <TodoItem v-for="todo in todos" :todo="todo" @remove="remove(todo.id)"/>
+  </ul>
+</template>
+
+<style scoped>
+  div > * {
+    padding: 1em;
+  }
+</style>
---
+++ TD3/projecttodos/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    props: {
+        todo: Object
+    },
+    methods: {
+        remove : function() {
+            this.$emit('remove', {id:this.todo.id});
+        }
+    },
+    emits: ['remove']
+}
+</script>
+
+<template>
+    <li>
+        <div class="checkbox" v-bind:class="{ 'alert alert-success': todo.checked}">
+            <input type="checkbox" v-model="todo.checked"></input>
+            {{ todo.text }}
+            <button @click = remove()>Supprimer</button>
+        </div>
+    </li>
+</template>
